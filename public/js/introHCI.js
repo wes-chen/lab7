cb73ef7d886f659ab03393e47e0db96fd3c8baed
Wesley Chen <35469114+wes-chen@users.noreply.github.com>
2021-02-18 10:36:01 -0800

    dumb stuff

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,10 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$('.likeBtn').click(likeClicked);
+}
+
+function likeClicked(e) {
+	console.log("Like clicked!");
+	ga("send", "event", 'like', 'click');
 }
